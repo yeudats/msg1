afef7c06324e63348fdf9d5003672bdfeccab698
yeudats <yehudatseva@gmail.com>
2025-05-27 19:56:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -27,4 +27,6 @@ def webhook():
 
 
 if __name__ == '__main__':
-    app.run()
+    import os
+    port = int(os.environ.get('PORT', 5000))  # Render תגדיר PORT כסביבת סביבה
+    app.run(host='0.0.0.0', port=port)
